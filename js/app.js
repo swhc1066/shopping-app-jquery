76a08dc92278295500800a2990448303dd7749f3
Sean Craig <fwcycling@gmail.com>
2015-06-16 18:13:00 -0500

    enter now adding text to list row

--- js/app.js
+++ js/app.js
@@ -3,15 +3,14 @@ $(document).ready(function () {
     /*Defining global variables*/
     console.log('Welcome to console');
     var submissions = 0;
-    var checkMark = '<span class="cross-off"></span>'
-    var xMark = '<span class="delete"></span>'
+
 
     /*Allows an "Enter" keystroke to work as a submit button for input field*/
     function getItem() {
         $('#list-imput').keydown(function (enter) {
             if (enter.keyCode == 13) {
                 postItem();
-                console.log("This worked")
+                console.log("Item added to the list")
                 $( this ).val( '' );
             }
         });
@@ -22,8 +21,8 @@ $(document).ready(function () {
     /*Takes returned value from previous function and manipulates it*/
     function postItem() {
         var item = $('#list-imput').val();
-        var work = '<p class="full-width no-strikethrough">'+ xMark + item + checkMark + '</p>';
-        $('#list-item').prepend(work);
+        var work = '<p class="list-text">' + item  + '</p>';
+        $('.list-item').prepend(work);
         $('#add-items').val('');
         console.log("This posted");
         /*$('#list-area p:first-child')
@@ -68,18 +67,4 @@ $(document).on("click", ".grey-delete-icon", function(){
     $(this).closest('.list-item').remove();
 });
 
-/*Allows user to check off items*/
-$(document).on("click", ".cross-off", function(){
-    if (!$(this).closest('p').hasClass('strikethrough')) {
-        console.log("I am going to add a strikethrough");
-        $(this).closest('p').addClass("strikethrough");
-        $(this).siblings('.delete').addClass("alt-delete");
-        $(this).addClass("alt-cross-off");
-   }
-   else {
-        console.log("I am going to remove a strikethrough");
-        $(this).closest('p').removeClass("strikethrough");
-        $(this).siblings('.delete').removeClass("alt-delete");
-        $(this).removeClass("alt-cross-off");
-   }
-});
+
